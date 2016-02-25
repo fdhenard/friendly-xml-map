eae2b17a7c5d19d85fd74992bf58a671781f596d
Frank Henard <fdhenard@gmail.com>
2016-02-25 16:46:42 -0600

    progress with 2nd approach

--- src/friendly_xml_map/core.clj
+++ src/friendly_xml_map/core.clj
@@ -1,6 +1,7 @@
 (ns friendly-xml-map.core
   (:require [clojure.xml :as xml]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [clojure.pprint :as pp]))
 
 ;; (defn foo
 ;;   "I don't do a whole lot."
@@ -18,14 +19,14 @@
 
 
 (defn friendly-map [xml-map schema]
-  (let [valid-attrs (filter #(and (contains? schema (first %))
+  (let [valid-attrs (filter #(and (contains? schema (first %)) ;; is the attr in the schema?
                                   true ;; also validate type?
                                   )
                             (:attrs xml-map))
         content-filter-func (fn [x]
-                              ;; (println "x = " x)
+                              (println "x = " x)
                               (and (map? x)
-                                   (contains? schema (:tag x))
+                                   (contains? schema (:tag x)) ;; is the tag in the schema?
                                    (= 1 (count (:content x)))))
         convert-content-to-key-val (fn [content-map]
                                      [(:tag content-map) (-> content-map :content first)])
@@ -37,3 +38,15 @@
         ;;                    (map convert-content-to-key-val))
         valid-content (into [] valid-content-xf (:content xml-map))]
     (into {} (concat valid-attrs valid-content))))
+
+
+(defn friendly-map-two [xml-map schema]
+  (let [conversion-func (fn [[attr-id attr-value]]
+                          (let [analy-map {:attr-id attr-id
+                                           :attr-value attr-value
+                                           :attrs-of-map (:attrs xml-map)}]
+                            (println (str "analy map = " (with-out-str (pp/pprint analy-map)))))
+                          (if (contains? (:attrs xml-map) attr-id) ;; should also check type?
+                            [attr-id (get (:attrs xml-map) attr-id)]))
+        key-val-vector (map conversion-func schema)]
+    (into {} key-val-vector)))
--- test/friendly_xml_map/core_test.clj
+++ test/friendly_xml_map/core_test.clj
@@ -1,7 +1,46 @@
 (ns friendly-xml-map.core-test
   (:require [clojure.test :refer :all]
-            [friendly-xml-map.core :refer :all]))
+            [friendly-xml-map.core :refer :all]
+            [schema.core :as s]
+            [clojure.data :refer [diff]]
+            [clojure.pprint :as pp]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; (deftest a-test
+;;   (testing "FIXME, I fail."
+;;     (is (= 0 1))))
+
+(defn- map-is-same? [map1 map2]
+  (let [the-diff (diff map1 map2)]
+    (and (nil? (first the-diff)) (nil? (nth the-diff 1)))))
+
+
+(deftest simple
+  (let [What {:attr-one s/Str
+              :when s/Str}
+        the-xml "<what attr-one=\"who\"><when>one</when></what>"
+        xml-map (parse-str the-xml)
+        friendly-map (friendly-map xml-map What)]
+    (is (map-is-same? friendly-map {:attr-one "who"
+                                    :when "one"}))))
+
+(deftest list-of-strings
+  (let [What {:attr-one s/Str
+              :attr-two [s/Str]}
+        the-xml "<what attr-one=\"who\"><attr-two>one</attr-two><attr-two>two</attr-two></what>"
+        xml-map (parse-str the-xml)
+        friendly-map (friendly-map xml-map What)]
+    (println (format "xml-map = %s" (with-out-str (pp/pprint xml-map))))
+    (println (format "friendly-map = %s" (with-out-str (pp/pprint friendly-map))))
+    (is (map-is-same? friendly-map {:attr-one "who"
+                                    :attr-two ["one", "two"]}))))
+
+(deftest list-of-strings-two
+  (let [What {:attr-one s/Str
+              :attr-two [s/Str]}
+        the-xml "<what attr-one=\"who\"><attr-two>one</attr-two><attr-two>two</attr-two></what>"
+        xml-map (parse-str the-xml)
+        friendly-map (friendly-map-two xml-map What)]
+    (println (format "xml-map = %s" (with-out-str (pp/pprint xml-map))))
+    (println (format "friendly-map = %s" (with-out-str (pp/pprint friendly-map))))
+    (is (map-is-same? friendly-map {:attr-one "who"
+                                    :attr-two ["one", "two"]}))))
